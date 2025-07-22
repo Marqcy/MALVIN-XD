9ca10eaa347502317b1e2b27d4213b4d92f54ee3
Marqcy <oluberoelphas@gmail.com>
2025-07-22 18:40:56 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " malvin~JqUHgLyI#9rWRnYWWfoEqQVSIl6OLDDRtpPZ0cMRqRnwjiXXIfTQ",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "254707418820", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
